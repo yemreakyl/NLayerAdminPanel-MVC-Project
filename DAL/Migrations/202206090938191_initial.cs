446ea2cf13c41558528b7ffefdf00c061de988e0
Yunus emre Akyol <yemreakyl@gmail.com>
2022-06-09 12:48:09 +0300

    Migrations

---
+++ DAL/Migrations/202206090938191_initial.cs
@@ -0,0 +1,71 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Active = c.Boolean(nullable: false),
+                        Deleted = c.Boolean(nullable: false),
+                        CreateTime = c.DateTime(nullable: false),
+                        Title = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Subcategories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CategoryId = c.Int(nullable: false),
+                        Active = c.Boolean(nullable: false),
+                        Deleted = c.Boolean(nullable: false),
+                        CreateTime = c.DateTime(nullable: false),
+                        Title = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Campaign = c.Boolean(nullable: false),
+                        CampaignRate = c.Int(nullable: false),
+                        SubcategoryId = c.Int(nullable: false),
+                        Active = c.Boolean(nullable: false),
+                        Deleted = c.Boolean(nullable: false),
+                        CreateTime = c.DateTime(nullable: false),
+                        Title = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Subcategories", t => t.SubcategoryId, cascadeDelete: true)
+                .Index(t => t.SubcategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "SubcategoryId", "dbo.Subcategories");
+            DropForeignKey("dbo.Subcategories", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "SubcategoryId" });
+            DropIndex("dbo.Subcategories", new[] { "CategoryId" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Subcategories");
+            DropTable("dbo.Categories");
+        }
+    }
+}
--- ENTİTİES/Models/Materials/Subcategory.cs
+++ ENTİTİES/Models/Materials/Subcategory.cs
@@ -5,7 +5,7 @@ namespace ENTİTİES.Materials
 {
     public class Subcategory : BaseEntity
     {
-        [ForeignKey("Category")]
+        
         public int CategoryId { get; set; }
         public virtual Category Categories { get; set; }
         public virtual ICollection<Product> Products { get; set; }
